b79eafe53e72681e2734965cb3c8b9a926f837a0
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-05-07 09:13:17 -0300

    começando a aplicar filtros

--- app/main.js
+++ app/main.js
@@ -1,28 +1,11 @@
 let livros = [];
 const endpointApi =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
-const elementoParaInserirLivros = document.getElementById("livros");
 getBuscarLivrosDaAPI();
 
 async function getBuscarLivrosDaAPI() {
   const res = await fetch(endpointApi);
   livros = await res.json();
-  console.log(livros);
-  exibirLivrosTela(livros);
-}
-
-function exibirLivrosTela(listaDeLivros) {
-  listaDeLivros.forEach((livro) => {
-    elementoParaInserirLivros.innerHTML += `  <div class="livro">
-       <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>`;
-  });
+  let livrosDesconto = aplicarDesconto(livros);
+  exibirLivrosTela(livrosDesconto);
 }
